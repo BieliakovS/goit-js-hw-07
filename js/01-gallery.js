6ad97c164765ac88c696687d98c1da20e2cf3ca0
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-02-28 00:31:09 +0200

    hw7/ 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+galleryEl.addEventListener("click", handleGallegyElClick);
+
+function createGalleryItemsMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    })
+    .join("");
+}
+
+function handleGallegyElClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}">
+`);
+
+  instance.show();
+}
